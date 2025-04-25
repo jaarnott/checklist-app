0a59bd8c19c1db9c7134c3166d8e07f571385448
jaarnott <ja@cookbrothersbars.co.nz>
2025-04-25 12:23:13 +1200

    Update Program.cs

--- LoadedChecklist/backend/Program.cs
+++ LoadedChecklist/backend/Program.cs
@@ -1,6 +1,13 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllers();
+
 var app = builder.Build();
+
 app.MapControllers();
+
 app.Run();
