6be472f1c6df13f37b3aa69540510fd90cf8eed5
jaarnott <ja@cookbrothersbars.co.nz>
2025-04-26 01:33:46 +0000

    update vite

--- LoadedChecklist/frontend/vite.config.js
+++ LoadedChecklist/frontend/vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../backend/wwwroot', // <--- critical for Azure to serve static files
-    emptyOutDir: true,
+    // Use an absolute path to avoid confusion with relative paths
+    outDir: path.resolve(__dirname, '../backend/wwwroot'), // Absolute path
+    emptyOutDir: true, // Ensure the build output directory is cleared before new build
   },
-})
+});
 
 
